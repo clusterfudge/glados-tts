5a70738d09e7d4ee26fec16f2d1380fb5a0c542a
Sean Fitzgerald <sean@fitzgeralds.me>
2024-04-17 17:10:28 -0700

    Improvements to the web server
    Get port from env var (compatible with most herokuish environments).
    Allow disabling cache via env var.
    Only write to disk if cache is enabled, else write to in-memory stream
    and then serve from buffer.

--- engine.py
+++ engine.py
@@ -1,81 +1,74 @@
 import sys
 import os
-sys.path.insert(0, os.getcwd()+'/glados_tts')
-
-import torch
-from utils.tools import prepare_text
-from scipy.io.wavfile import write
-import time
-
+import io
+from hashlib import sha256
+sys.path.insert(0, os.getcwd() + '/glados_tts')
 from glados import tts_runner
-		
+
 print("\033[1;94mINFO:\033[;97m Initializing TTS Engine...")
 
 glados = tts_runner(False, True)
 
+
+def cache_filename(utterance, root):
+    h = sha256(utterance).hexdigest()
+    return os.path.join(root, f"{h}.wav")
+
+
+def cache(utterance, audio, root=os.path.join(os.getcwd(), 'cache')):
+    cache_file = cache_filename(utterance, root)
+    with open(os.path.join(root, f"{h}.wav"), 'wb') as f:
+        audio.seek(0)
+        f.write(audio.read())
+
+
+def from_cache(utterance, root=os.path.join(os.getcwd(), 'cache')):
+    cache_file = cache_filename(utterance, root)
+    if os.path.exists(cache_file):
+        with open(cache_file, 'rb') as f:
+            return f.read()
+    return None
+
+
 def glados_tts(text, key=False, alpha=1.0):
-
-	if(key):
-		output_file = ('audio/GLaDOS-tts-temp-output-'+key+'.wav')
-	else:
-		output_file = ('audio/GLaDOS-tts-temp-output.wav')
-
-	glados.run_tts(text, alpha).export(output_file, format = "wav")
-	return True
+    output_buffer = io.BytesIO()
+    glados.run_tts(text, alpha).export(output_buffer, format="wav")
+    output_buffer.seek(0)
+    return output_buffer
 
 
 # If the script is run directly, assume remote engine
 if __name__ == "__main__":
-	
-	# Remote Engine Veritables
-	PORT = 8124
-	CACHE = True
 
-	from flask import Flask, request, send_file
-	import urllib.parse
-	import shutil
-	
-	print("\033[1;94mINFO:\033[;97m Initializing TTS Server...")
-	
-	app = Flask(__name__)
+    # Remote Engine Variables
+    PORT = os.getenv("PORT") or 8124
+    CACHE = os.getenv("CACHE") != "FALSE"
 
-	@app.route('/synthesize/', defaults={'text': ''})
-	@app.route('/synthesize/<path:text>')
-	def synthesize(text):
-		if(text == ''): return 'No input'
-		
-		line = urllib.parse.unquote(request.url[request.url.find('synthesize/')+11:])
-		filename = "GLaDOS-tts-"+line.replace(" ", "-")
-		filename = filename.replace("!", "")
-		filename = filename.replace("°c", "degrees celcius")
-		filename = filename.replace(",", "")+".wav"
-		file = os.getcwd()+'/audio/'+filename
-		
-		# Check for Local Cache
-		if(os.path.isfile(file)):
-		
-			# Update access time. This will allow for routine cleanups
-			os.utime(file, None)
-			print("\033[1;94mINFO:\033[;97m The audio sample sent from cache.")
-			return send_file(file)
-			
-		# Generate New Sample
-		key = str(time.time())[7:]
-		if(glados_tts(line, key)):
-			tempfile = os.getcwd()+'/audio/GLaDOS-tts-temp-output-'+key+'.wav'
-						
-			# If the line isn't too long, store in cache
-			if(len(line) < 200 and CACHE):
-				shutil.move(tempfile, file)
-			else:
-				return send_file(tempfile)
-				os.remove(tempfile)
-				
-			return send_file(file)
-				
-		else:
-			return 'TTS Engine Failed'
-			
-	cli = sys.modules['flask.cli']
-	cli.show_server_banner = lambda *x: None
-	app.run(host="0.0.0.0", port=PORT)
+    from flask import Flask, Response, request
+
+    print("\033[1;94mINFO:\033[;97m Initializing TTS Server...")
+
+    app = Flask(__name__)
+
+
+    @app.route('/synthesize', methods=['POST'])
+    def synthesize():
+        text = request.get_data(as_text=True)
+        if not text:
+            return 'No input', 400
+
+        result = b''
+        if CACHE:
+            result = from_cache(text)
+
+        if not result:
+            # Generate New Sample
+            audio_buffer = glados_tts(text)
+            result = audio_buffer.read()
+
+        return Response(result, mimetype="audio/wav")
+
+
+    cli = sys.modules['flask.cli']
+    cli.show_server_banner = lambda *x: None
+    app.run(host="0.0.0.0", port=PORT)
